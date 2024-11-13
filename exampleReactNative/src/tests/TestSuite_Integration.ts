ad885a07158e26cc87b80779c4c47d30b5f87692
Jan Kobersky <kober32@gmail.com>
2024-11-13 12:28:31 +0100

    Fixed tests

--- exampleReactNative/src/tests/TestSuite_Integration.ts
+++ exampleReactNative/src/tests/TestSuite_Integration.ts
@@ -17,7 +17,7 @@
 import { PowerAuth, PowerAuthAuthentication } from 'react-native-powerauth-mobile-sdk';
 import { TestSuite } from './TestSuite';
 import { IntegrationUtils } from './utils/IntegrationUtils';
-import { WultraMobileToken, WMTQROperationParser, WMTUserAgent, WMTSigningKey } from 'react-native-mtoken-sdk';
+import { WultraMobileToken, WMTQROperationParser, WMTUserAgent, WMTSigningKey, WMTKnownRestApiError } from 'react-native-mtoken-sdk';
 
 export class TestSuite_Integration extends TestSuite {
 
@@ -69,6 +69,23 @@ export class TestSuite_Integration extends TestSuite {
         this.assertNull(resp.responseError, "Response error should be null after successful auth")
     }
 
+    async testRepeatedApprovePayment() {
+        const op = await this.utils.createOperation()
+        
+        const operation = (await this.mtoken.operations.detail(op.operationId)).responseObject!!
+
+        // authorize the operation
+        const auth = PowerAuthAuthentication.password(this.pin)
+        const resp = await this.mtoken.operations.authorize(operation, auth)
+        this.assertEquals(resp.status, "OK", "Missing response object after successful auth")
+        this.assertNull(resp.responseError, "Response error should be null after successful auth")
+
+        // repeated authorization should result in error
+        const respRepeated = await this.mtoken.operations.authorize(operation, auth)
+        this.assertEquals(respRepeated.responseError?.code, WMTKnownRestApiError.OperationAlreadyFinished, "Missing response object after successful auth")
+        this.assertEquals(respRepeated.status, "ERROR", "Status should be ERROR after error")
+    }
+
     async testRejectPayment() {
         const op = await this.utils.createOperation()
         const operations = await this.mtoken.operations.pendingList()
@@ -89,6 +106,7 @@ export class TestSuite_Integration extends TestSuite {
         const opRecord = history.responseObject!!.find( it => it.id == op.operationId )
         this.assertNotNull(opRecord)
         this.assertEquals(opRecord?.status, "PENDING")
+        this.assertEquals(opRecord?.id, op.operationId, "Different operationId than the one retrieved from the server")
     }
 
     async testQROperation() {
