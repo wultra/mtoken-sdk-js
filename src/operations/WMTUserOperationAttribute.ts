098e5a03ecc6fc5eadd98dae13b0eaa8f9f5e167
Marek Chrenko <marek.chrenko@wultra.com>
2025-08-11 17:52:50 +0200

    Features/31 add alert attribute type (#45)
    * add WMTOperationAttributeAlert type
    * Update src/operations/WMTUserOperationAttribute.ts

--- src/operations/WMTUserOperationAttribute.ts
+++ src/operations/WMTUserOperationAttribute.ts
@@ -18,7 +18,7 @@
  * Operation Attribute can be visualized as "1 row in operation screen".
  * 
  * `MobileTokenOperationAttribute` is considered to be "abstract".
- * Every type of the attribute has it's own "strongly typed" implementation.
+ * Every type of the attribute has its own "strongly typed" implementation.
  */
 export interface WMTUserOperationAttribute {
 
@@ -31,7 +31,7 @@ export interface WMTUserOperationAttribute {
     /** 
      * Type of the operation.
      * 
-     * If the type is for example `MobileTokenAttributeType.Amount`, you can retype the instance to `MobileTokenOperationAttributeAmount`.
+     * If the type is, for example, `MobileTokenAttributeType.Amount`, you can retype the instance to `MobileTokenOperationAttributeAmount`.
      * 
      * The possible string value is a fallback for unknown attribute types.
      */
@@ -41,11 +41,11 @@ export interface WMTUserOperationAttribute {
     label: string
 }
 
-/** Attribute type. Based on this type, proper class should be chosen for "deserialization". */
+/** Attribute type. Based on this type, a proper class should be chosen for "deserialization". */
 export enum WMTAttributeType {
     /** Amount, like "100.00 CZK." */
     AMOUNT            = "AMOUNT", 
-    /** Currency conversion, for example when changing money from USD to EUR. */
+    /** Currency conversion, for example, when changing money from USD to EUR. */
     AMOUNT_CONVERSION = "AMOUNT_CONVERSION",
     /** Any key value pair. */
     KEY_VALUE          = "KEY_VALUE",
@@ -54,17 +54,19 @@ export enum WMTAttributeType {
     /** Single highlighted text, written in a larger font, used as a section heading. */
     HEADING           = "HEADING",
     /** For image displaying. */
-    IMAGE            = "IMAGE"
+    IMAGE            = "IMAGE",
+    /** Alert with type SUCCESS, INFO, WARNING or ERROR. Each type has an associated icon. */
+    ALERT            = "ALERT"
 }
 
-/** Amount attribute is 1 row in operation, that represents "Payment Amount". */
+/** Amount attribute is 1 row in operation that represents "Payment Amount". */
 export interface WMTOperationAttributeAmount extends WMTUserOperationAttribute {
     
     /**
      * Formatted amount for presentation.
      * 
      * This property will be properly formatted based on the response language.
-     * For example when amount is 100 and the acceptLanguage is "cs" for czech,
+     * For example, when the amount is 100 and the acceptLanguage is "cs" for czech,
      * the amountFormatted will be "100,00".
      */ 
     amountFormatted?: string
@@ -72,7 +74,7 @@ export interface WMTOperationAttributeAmount extends WMTUserOperationAttribute {
     /**
      * Formatted currency to the locale based on acceptLanguage.
      * 
-     * For example when the currency is CZK, this property will be "Kč".
+     * For example, when the currency is CZK, this property will be "Kč".
      */
     currencyFormatted?: string
     
@@ -123,14 +125,14 @@ export interface WMTOperationAttributeImage extends WMTUserOperationAttribute {
     originalUrl?: string
 }
 
-/** Conversion attribute is 1 row in operation, that represents "Money Conversion". */
+/** Conversion attribute is 1 row in operation that represents "Money Conversion". */
 export interface WMTOperationAttributeAmountConversion extends WMTUserOperationAttribute {
     
     /**
      * If the conversion is dynamic and the application should refresh it periodically.
      * 
-     * This is just a hint for the application UI. This SDK does not offer feature to periodically
-     * refresh conversion rate.
+     * This is just a hint for the application UI. This SDK does not offer a feature to periodically
+     * refresh the conversion rate.
      */
     dynamic: boolean 
     
@@ -138,21 +140,21 @@ export interface WMTOperationAttributeAmountConversion extends WMTUserOperationA
      * Formatted amount for presentation.
      * 
      * This property will be properly formatted based on the response language.
-     * For example when amount is 100 and the acceptLanguage is "cs" for czech,
+     * For example, when the amount is 100 and the acceptLanguage is "cs" for czech,
      * the amountFormatted will be "100,00".
      */
     sourceAmountFormatted?: string 
     /**
      * Formatted currency to the locale based on acceptLanguage.
      * 
-     * For example when the currency is CZK, this property will be "Kč".
+     * For example, when the currency is CZK, this property will be "Kč".
      */
     sourceCurrencyFormatted?: string
 
     /**
      * Payment amount.
      * 
-     * Amount might not be precise (due to floating point conversion during deserialization from json)
+     * Amount might not be precise (due to floating point conversion during deserialization from JSON)
      * use amountFormatted property instead when available.
      */
     sourceAmount?: number
@@ -171,7 +173,7 @@ export interface WMTOperationAttributeAmountConversion extends WMTUserOperationA
      * Formatted amount for presentation.
      * 
      * This property will be properly formatted based on the response language.
-     * For example when amount is 100 and the acceptLanguage is "cs" for czech,
+     * For example, when the amount is 100 and the acceptLanguage is "cs" for czech,
      * the amountFormatted will be "100,00".
      */
     targetAmountFormatted?: string
@@ -179,13 +181,13 @@ export interface WMTOperationAttributeAmountConversion extends WMTUserOperationA
     /**
      * Formatted currency to the locale based on acceptLanguage.
      * 
-     * For example when the currency is CZK, this property will be "Kč".
+     * For example, when the currency is CZK, this property will be "Kč".
      */
     targetCurrencyFormatted?: string
     /**
      * Payment amount.
      * 
-     * Amount might not be precise (due to floating point conversion during deserialization from json)
+     * Amount might not be precise (due to floating point conversion during deserialization from JSON)
      * use amountFormatted property instead when available.
      */
     targetAmount?: number
@@ -200,4 +202,48 @@ export interface WMTOperationAttributeAmountConversion extends WMTUserOperationA
      * or "EUR" behind the amount depending on locale.
      */
     targetValueFormatted?: string
+}
+
+
+/**
+ * Allowed types for Alert type.
+ */
+export enum WMTAttributeAlertType {
+    /** Success alert type. */
+    success = "SUCCESS",
+    /** Info alert type. */
+    info = "INFO",
+    /** Warning alert type. */
+    warning = "WARNING",
+    /** Error alert type. */
+    error = "ERROR"
+}
+
+/** 
+ * Alert attribute that represents a notification or message to the user.
+ */
+export interface WMTOperationAttributeAlert extends WMTUserOperationAttribute {
+
+    /**
+     * Type of the alert.
+     *
+     * Each alert type should have an associated icon on the client side.
+     */
+    alertType: WMTAttributeAlertType
+
+    /**
+     * Title of the alert. Optional.
+     * 
+     * When specified along with a message, it should be displayed as a highlighted part of the text
+     * (i.e., bold and above the message).
+     */
+    title?: string
+
+    /**
+     * Message content of the alert.
+     * 
+     * If both message and title are specified, the message is displayed as the regular text below the title.
+     * If only a message is specified, it is displayed as a regular text.
+     */
+    message: string
 }
