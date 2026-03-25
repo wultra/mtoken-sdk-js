82bdcd87c1277582a83c355c0ebc9334c870fae4
Marek Stransky <stranskymarek90@gmail.com>
2026-03-25 14:22:35 +0100

    Revert incorrectly commited files

--- exampleReactNative/metro.config.js
+++ exampleReactNative/metro.config.js
@@ -1,8 +1,4 @@
-const path = require('path');
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
-
-const projectRoot = __dirname;
-const monorepoRoot = path.resolve(projectRoot, '..');
 
 /**
  * Metro configuration
@@ -10,14 +6,6 @@ const monorepoRoot = path.resolve(projectRoot, '..');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {
-  watchFolders: [monorepoRoot],
-  resolver: {
-    nodeModulesPaths: [
-      path.resolve(projectRoot, 'node_modules'),
-      path.resolve(monorepoRoot, 'node_modules'),
-    ],
-  },
-};
+const config = {};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
